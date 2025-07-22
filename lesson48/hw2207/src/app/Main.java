7821b1e55a4eaf942b20e3cdfbf34fdc1c6da4ea
RBcuber <igor.kirichenko.07@gmail.com>
2025-07-23 00:03:22 +0200

    aфÐdd method with map

--- lesson48/hw2207/src/app/Main.java
+++ lesson48/hw2207/src/app/Main.java
@@ -10,11 +10,21 @@ public class Main {
         String str1 = "({[]})";
         String str2 = "{} {{gsdgghs{{} }}gj{[][  ][hg{} [[fg{sg]g]  ]]] {{ g]]  dd(g(((  ) ))()(()gsd() ())){asd}gsd} { )() ()))g(((} ";
         String str3 = "[d{ddfghgj (d)sassd g ajsd}d([j fgadasdd asd] asd){d}]";
+        String str4 = "}[d{ddfghgj (d)sassd g ajsd}d([j fgadasdd asd] asd){d}]";
 
 
+        System.out.println("Через метод bracketsPaar");
         System.out.println(isValid(str1));
         System.out.println(isValid(str2));
         System.out.println(isValid(str3));
+        System.out.println(isValid(str4));
+
+
+        System.out.println("Через map");
+        System.out.println(isValidMap(str1));
+        System.out.println(isValidMap(str2));
+        System.out.println(isValidMap(str3));
+        System.out.println(isValidMap(str4));
 
 
     }
@@ -41,8 +51,33 @@ public class Main {
         return true;
     }
 
+    private static boolean isValidMap(String str) {
+        List<Character> list = new ArrayList<>();
+        Map<Character, Character> map = Map.of(
+                '(', ')',
+                '[', ']',
+                '{', '}'
+        );
+        for (int i = 0; i < str.length(); i++) {
+            if (map.containsKey(str.charAt(i))) {
+                list.add(str.charAt(i));
+            } else if (map.containsValue(str.charAt(i))) {
+                if (    list.isEmpty()
+                        || map.get(list.get(list.size() - 1)) != str.charAt(i))
+                 {
+                    return false;
+                }
+                list.remove(list.size() - 1);
+            }
+        }
+        return true;
+    }
+
+
     private static boolean bracketsPaar(char o, char c) {
         return (o == '(' && c == ')') || (o == '[' && c == ']') || (o == '{' && c == '}');
     }
+
+
 }
 
